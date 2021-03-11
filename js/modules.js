ca352e3269f3a084c333e145f602fee16eb24201
johncummins <johncummins1997@gmail.com>
2021-03-11 14:03:43 +0000

    First Commit

---
+++ js/modules.js
@@ -0,0 +1,12 @@
+// function moduleInfo() {
+// 	// window.alert("hello");
+// 	//getting the stored item back
+// 	if (num == 1) {
+// 		var modStoredValue1 = window.localStorage.getItem("moduleName1");
+// 		document.getElementById("mod_title").innerHTML = modStoredValue1;
+// 		// $("#mod_title").text(modStoredValue1);
+// 	} else {
+// 		var modStoredValue2 = window.localStorage.getItem("moduleName2");
+// 		document.getElementById("module_title").innerHTML = modStoredValue2;
+// 	}
+// }
